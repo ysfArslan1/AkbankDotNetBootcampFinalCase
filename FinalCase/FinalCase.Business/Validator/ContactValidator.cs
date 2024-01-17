a9a234e42a3eb77f67807d6bae23ad2fb73a9135
ysfArslan1 <ysf.arslan2017@gmail.com>
2024-01-17 21:14:40 +0300

    Create ContactValidator.cs

---
+++ FinalCase/FinalCase.Business/Validator/ContactValidator.cs
@@ -0,0 +1,30 @@
+﻿using FinalCase.Schema;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalCase.Business.Validator
+{
+    public class CreateContactValidator : AbstractValidator<ContactRequest>
+    {
+        public CreateContactValidator()
+        {
+            RuleFor(x => x.UserId).NotNull().NotEmpty().GreaterThan(0);
+            RuleFor(x => x.Email).NotNull().NotEmpty().MaximumLength(100);
+            RuleFor(x => x.Phone).NotNull().NotEmpty().MaximumLength(100);
+        }
+    }
+
+    public class UpdateContactValidator : AbstractValidator<ContactRequest>
+    {
+        public UpdateContactValidator()
+        {
+            RuleFor(x => x.UserId).NotNull().NotEmpty().GreaterThan(0);
+            RuleFor(x => x.Email).NotNull().NotEmpty().MaximumLength(100);
+            RuleFor(x => x.Phone).NotNull().NotEmpty().MaximumLength(100);
+        }
+    }
+}
